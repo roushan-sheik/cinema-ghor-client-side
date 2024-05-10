c0f2e7354e7faf40c5ec2e771c020a24da9e9f6a
roushan-sheik <roushansheik@gmail.com>
2024-05-10 21:19:10 +0600

    eslint error fixed

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h2>heading 2 text</h2>
+    </div>
+  );
 };
 
 export default Header;
