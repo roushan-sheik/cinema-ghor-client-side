0f2036e495f758ff6d282670e2a7b0c26e23c639
roushan-sheik <roushansheik@gmail.com>
2024-05-11 05:53:05 +0600

    error page added

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Home from "../pages/home/Home";
 import Login from "../pages/logIn/Login";
 import Register from "../pages/register/Register";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
